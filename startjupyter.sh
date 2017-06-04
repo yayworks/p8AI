4c295a212b31be3236927fe1660e45bdf647ac26
yayworks <raj@yayworks.com>
2017-06-03 23:02:08 -0500

    Update startjupyter.sh

--- startjupyter.sh
+++ startjupyter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source /opt/DL/caffe-ibm/bin/caffe-activate
 source /opt/DL/openblas/bin/openblas-activate
-source /opt/DL/tensorflow/bin/tensorflow-activate
+#source /opt/DL/tensorflow/bin/tensorflow-activate
+source /root/anaconda3/bin/activate tensorflow
 source /opt/DL/theano/bin/theano-activate
 source /opt/DL/torch/bin/torch-activate
 source /opt/DL/digits/bin/digits-activate
