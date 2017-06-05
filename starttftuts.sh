acafe23ac7eabf3ddeb49623337c0fa53974cf6a
yayworks <raj@yayworks.com>
2017-06-05 16:01:22 -0500

    Update starttftuts.sh

--- starttftuts.sh
+++ starttftuts.sh
@@ -3,7 +3,9 @@
 #source /opt/DL/openblas/bin/openblas-activate
 #source /opt/DL/tensorflow/bin/tensorflow-activate
 #source /opt/DL/digits/bin/digits-activate
+
+export PYTHONPATH=/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/
 source /root/anaconda3/bin/activate tensorflow
 cd /opt/DL/Tensorflow-Tutorials
 
-jupyter notebook --ip=0.0.0.0 --port=$1 --allow-root &
+/root/anaconda3/bin/jupyter  notebook --ip=0.0.0.0 --port=$1 --allow-root  &
