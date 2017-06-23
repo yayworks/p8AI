abb159eddc487f7fa977d7bac08d0dc8a0ec09fa
yayworks <raj@yayworks.com>
2017-06-23 15:13:18 -0500

    Update starttftuts.sh

--- starttftuts.sh
+++ starttftuts.sh
@@ -4,7 +4,9 @@
 #source /opt/DL/tensorflow/bin/tensorflow-activate
 #source /opt/DL/digits/bin/digits-activate
 
-export PYTHONPATH=/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/
+#/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages', '/root', '/root/anaconda3/envs/tensorflow/lib/python36.zip', '/root/anaconda3/envs/tensorflow/lib/python3.6', '/root/anaconda3/envs/tensorflow/lib/python3.6/lib-dynload', '/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/prettytensor-0.7.2-py3.6.egg', '/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/enum34-1.1.6-py3.6.egg', '/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/setuptools-27.2.0-py3.6.egg
+export PYTHONPATH=/root/anaconda3/envs/tensorflow/lib/python3.6:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/prettytensor-0.7.2-py3.6.egg:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/enum34-1.1.6-py3.6.egg:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/setuptools-27.2.0-py3.6.egg
+
 source /root/anaconda3/bin/activate tensorflow
 cd /opt/DL/Tensorflow-Tutorials
 
